20a8905004c99d62a0ebfa840c5a4f1ecdc00b08
ayukha <ayussh.khare@gmail.com>
2018-07-08 22:49:02 +0530

    state variable used

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css'
 import Person from './Person/Person'
 
 class App extends Component {
+	state = {
+		persons: [
+		{name: 'max', age: '28'},
+		{name: 'manu', age: '38'},
+		{name: 'steffanie', age: '38'},
+
+
+		]
+	}
+
   render () {
     return (
       <div className='App'>
         <h1> Hi, I am an app</h1>
-        <Person name='max' age='23' />
-        <Person name='manu' age='33' />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
       </div>
     )
   }

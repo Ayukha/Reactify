760d2c643b925e8bcb465eedf33496404882db58
ayukha <ayussh.khare@gmail.com>
2018-07-11 00:48:47 +0530

    typo fixed

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const person = (props) => {
   return (
-    <p onClick={props.click}> I'm a {props.name} and i am {props.age} not {Math.floor(Math.random() * 30)} </p>
+    <p onClick={props.click}> I am a {props.name} and i am {props.age} not {Math.floor(Math.random() * 30)} </p>
     <input type="text" onChange={props.changed}></input>
   	)
 }

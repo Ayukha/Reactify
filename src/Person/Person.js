dfe9f603d12660bf9c0e9ab408988477454aae6b
ayukha <ayussh.khare@gmail.com>
2018-07-11 00:53:48 +0530

    data binding fixed

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const person = (props) => {
   return (
-    <p onClick={props.click}> I am a {props.name} and i am {props.age} not {Math.floor(Math.random() * 30)} </p>
-    <input type="text" onChange={props.changed}></input>
+    <div>
+      <p onClick={props.click}> I am a {props.name} and i am {props.age} not {Math.floor(Math.random() * 30)} </p>
+      <input type='text' onChange={props.changed} />
+    </div>
   	)
 }
 
